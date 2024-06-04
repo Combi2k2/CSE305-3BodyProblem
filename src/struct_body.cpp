57e2d16b969e3ac6773ffe539c71e5d97100b8dc
ngoctu-nguyen <ng.ngoctu1510@gmail.com>
2024-06-04 09:17:04 +0200

    Adjust operator += and add operator /=

--- src/struct_body.cpp
+++ src/struct_body.cpp
@@ -1,24 +1,35 @@
 #include "../include/struct_body.h"
 
-Body operator + (const Body &a, const Body &b) {
+Body operator+(const Body &a, const Body &b)
+{
     return Body(
         a.mass + b.mass,
         a.radius + b.radius,
-        a.center[0] + b.center[0],
-        a.center[1] + b.center[1],
-        a.speed[0] + b.speed[0],
-        a.speed[1] + b.speed[1]
-    );
+        (a.center[0] * a.mass + b.center[0] * b.mass) / (a.mass + b.mass),
+        (a.center[1] * a.mass + b.center[1] * b.mass) / (a.mass + b.mass),
+        (a.speed[0] * a.mass + b.speed[0] * b.mass) / (a.mass + b.mass),
+        (a.speed[1] * a.mass + b.speed[1] * b.mass) / (a.mass + b.mass));
 }
-void operator += (Body &a, const Body &b) {
+void operator+=(Body &a, const Body &b)
+{
     a.mass += b.mass;
     a.radius += b.radius;
-    a.center += b.center;
-    a.speed += b.speed;
+    a.center = (a.center * a.mass + b.center * b.mass) / (a.mass);
+    a.speed += (a.speed * a.mass + b.speed * b.mass) / (a.mass);
 }
-void operator *= (Body &a, const double &factor) {
+
+void operator*=(Body &a, const double &factor)
+{
     a.mass *= factor;
     a.radius *= factor;
     a.center *= factor;
     a.speed *= factor;
+}
+
+void operator/=(Body &a, const double &factor)
+{
+    a.mass /= factor;
+    a.radius /= factor;
+    a.center /= factor;
+    a.speed /= factor;
 }
