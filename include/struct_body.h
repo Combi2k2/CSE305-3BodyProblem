aed01efda11aaffe50c2454636ae1d2b59c88e21
ngoctu-nguyen <ng.ngoctu1510@gmail.com>
2024-06-10 07:50:04 +0200

    feat: redefine struct body given new struct particle

--- include/struct_body.h
+++ include/struct_body.h
@@ -2,38 +2,33 @@
 #define STRUCT_BODY_H
 
 #include "struct_vector.h"
+#include "struct_particle.h"
 
-struct Body
-{
-    double mass;
+struct Body {
+    Particle center;
     double radius;
-    Vector center;
     Vector speed;
 
-    explicit Body(double mass = 1, double r = 1, double x = 0, double y = 0, double dx = 0, double dy = 0)
-    {
-        this->mass = mass;
+    Body(double m = 1, double r = 1, double x = 0, double y = 0, double dx = 0, double dy = 0) {
+        this->speed  = Vector(dx, dy);
+        this->center = Particle(m, x, y);
         this->radius = r;
-        this->center = Vector(x, y);
-        this->speed = Vector(dx, dy);
     }
-    Body &operator=(const Body &other)
-    {
+    Body &operator=(const Body &other) {
         if (this == &other)
             return *this;
-
-        this->mass = other.mass;
+        
         this->center = other.center;
         this->radius = other.radius;
-        this->speed = other.speed;
+        this->speed  = other.speed;
 
         return *this;
     }
 };
 
-Body operator+(const Body &a, const Body &b);
-void operator+=(Body &a, const Body &b);
-void operator*=(Body &a, const double &factor);
-void operator/=(Body &a, const double &factor);
+Body operator + (const Body &a, const Body &b);
+void operator += (Body &a, const Body &b);
+void operator *= (Body &a, const double &factor);
+void operator /= (Body &a, const double &factor);
 
 #endif
